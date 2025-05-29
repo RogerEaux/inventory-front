d7f3a52373c54812bb857b0830ef22566ddaa4c6
rmere <rmere@griddynamics.com>
2025-05-29 13:00:36 -0600

    Add vitest and rtl for testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tailwindcss from '@tailwindcss/vite';
@@ -14,4 +14,9 @@ export default defineConfig({
       include: '**/*.svg?react',
     }),
   ],
-});
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.js',
+  },
+} as UserConfig);
