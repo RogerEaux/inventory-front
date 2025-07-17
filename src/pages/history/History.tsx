173d4bb3f11e1ccb7f218df13ece4021f95fb2cb
rmere <rmere@griddynamics.com>
2025-07-17 16:08:34 -0600

    Add history page

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,3 +1,14 @@
+import HistoryTable from './history-table/HistoryTable';
+
 export default function History() {
-  return <></>;
+  return (
+    <main className="flex flex-col items-center">
+      <section className="w-full max-w-4/5 px-8 py-16 max-sm:max-w-full">
+        <h1 className="mt-5 p-16 text-3xl font-bold text-nowrap max-sm:text-2xl">
+          History
+        </h1>
+        <HistoryTable />
+      </section>
+    </main>
+  );
 }
